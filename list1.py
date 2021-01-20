11b77d38ef9b0ebd9a9d34b71c10fd6d82e8ed52
KetnaSontakke <76588164+KetnaSontakke@users.noreply.github.com>
2021-01-20 16:40:59 +0530

    Create list1.py

---
+++ list1.py
@@ -0,0 +1,9 @@
+ 
+list1 = [12, -7, 5, 64, -14]          # list of numbers
+  
+
+for number in list1:                  # iterating each number in list 
+      
+     
+    if number >= 0:                   # checking condition
+       print(number, end = " ") 
